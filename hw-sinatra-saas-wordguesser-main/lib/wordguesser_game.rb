253ecc252856d4e00105df29a4aaaf29ebaa58b8
jesusdd <jesushe99@gmail.com>
2026-05-01 12:06:10 -0500

    Resulta hasta pasar 7 tests, spec linea 69

--- hw-sinatra-saas-wordguesser-main/lib/wordguesser_game.rb
+++ hw-sinatra-saas-wordguesser-main/lib/wordguesser_game.rb
@@ -6,7 +6,58 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+
+  def word
+    @word
+  end
+
+  def guesses
+    @guesses
+  end
+
+  def wrong_guesses
+    @wrong_guesses
+  end
+
+  def guess(a_letter)
+    if is_valid_guess(a_letter)
+      handle_guess_correctness(a_letter)
+      return true
+    else
+      return false
+    end
+  end
+
+
+  def handle_guess_correctness(a_letter)
+    if @guesses.include? a_letter or @wrong_guesses.include? a_letter
+      return
+    elsif @word.include? a_letter
+      @guesses = @guesses + a_letter
+    else
+      @wrong_guesses = @wrong_guesses + a_letter
+    end
+  end
+
+  def is_valid_guess(a_letter)
+    if a_letter.class != String
+      return false
+    end
+
+    if a_letter.length != 1
+      return false
+    end
+
+    /[a-zA-Z]/.match? a_letter
+    
+  end
+
+
+
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
